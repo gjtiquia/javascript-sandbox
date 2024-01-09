e16d81d6fb7b93e93afe5d8aaa13759074fc37aa
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-09 11:39:24 +0800

    WIP: add todos

--- packages/test-editor/node-runtime/src/index.ts
+++ packages/test-editor/node-runtime/src/index.ts
@@ -4,6 +4,9 @@ import path from "path";
 const app: Express = express();
 const PORT = 3000;
 
+// TODO : Probably... no need to serve frontend via express, can probably just serve frontend and backend separately on different ports and just connect frontend to backend using REST api calls
+// TODO : That way, frontend just need to concern with hosting itself (can use stuff like Vite for Hot Module Replacement), backend just need to concern with file system stuff.
+
 app.use("/", express.static(path.join(__dirname, '../../frontend')));
 
 app.listen(PORT, () => {
