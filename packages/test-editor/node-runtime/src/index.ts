8582ffdb316e83b2d96a5c956c0e0b62ad8c6d19
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-10 09:26:48 +0800

    feat: setup vite + react as frontend

--- packages/test-editor/node-runtime/src/index.ts
+++ packages/test-editor/node-runtime/src/index.ts
@@ -1,13 +1,7 @@
 import express, { Express, Request, Response } from "express";
-import path from "path";
 
 const app: Express = express();
 const PORT = 3000;
-
-// TODO : Probably... no need to serve frontend via express, can probably just serve frontend and backend separately on different ports and just connect frontend to backend using REST api calls
-// TODO : That way, frontend just need to concern with hosting itself (can use stuff like Vite for Hot Module Replacement), backend just need to concern with file system stuff.
-
-app.use("/", express.static(path.join(__dirname, '../../frontend')));
 
 app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`);
