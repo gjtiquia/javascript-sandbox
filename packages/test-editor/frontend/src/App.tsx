7ccfa3365ec854420c879405d566c4d83cdd766b
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-10 10:09:35 +0800

    feat: setup tailwind and basic styling

--- packages/test-editor/frontend/src/App.tsx
+++ packages/test-editor/frontend/src/App.tsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ReactNode, useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [path, setPath] = useState("");
+  const [files, setFiles] = useState(["No files"])
+
+  function onRefreshClicked() {
+    setFiles(["testing"])
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="p-5 flex flex-col gap-5">
+      <h1 className="font-bold text-3xl text-slate-100 text-center">
+        Game Engine Editor
+      </h1>
+
+      <div className="p-3 bg-slate-700 rounded-lg flex flex-col gap-2">
+        <h2 className="font-bold text-xl text-slate-100">
+          Files
+        </h2>
+
+        <div className="flex bg-slate-800 rounded-md pl-2">
+          <p className="text-slate-500">
+            Path:
+          </p>
+          <input
+            className="bg-slate-800 pl-1 rounded-md w-full text-slate-100"
+            onChange={e => setPath(e.target.value)}
+            value={path}
+          />
+
+          <button
+            className="text-slate-600 hover:text-slate-500 px-2"
+            onClick={(onRefreshClicked)}
+          >
+            Refresh
+          </button>
+        </div>
+
+        <ul>
+          {files.map(file => <ListElement>{file}</ListElement>)}
+        </ul>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  )
+}
+
+function ListElement({ children }: { children: ReactNode }) {
+  return (
+    <li className="bg-slate-800 text-slate-100 px-2 pb-1 first:pt-1 first:rounded-t-md last:rounded-b-md">
+      {children}
+    </li>
   )
 }
 
