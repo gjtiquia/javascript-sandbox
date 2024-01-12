1ca9514b8a693fc3d4399d97e10e3ef75d051749
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-12 10:05:25 +0800

    refactor: test-game index is for exporting, main is for running CLI game

---
+++ packages/test-game/src/main.ts
@@ -0,0 +1,50 @@
+import { TextCanvas } from "./TextCanvas";
+import { startListeningToKeypressEvents } from "./KeypressListener";
+import { Player } from "./Player";
+
+const _textCanvas = new TextCanvas(30, 10);
+
+// TODO : Probably list of game objects? might be overkill tho, we'll see
+const _player = new Player();
+
+function main() {
+    render();
+
+    const keypressEvents = startListeningToKeypressEvents();
+    keypressEvents.onUpPressed.addListener(onUpPressed);
+    keypressEvents.onDownPressed.addListener(onDownPressed);
+    keypressEvents.onLeftPressed.addListener(onLeftPressed);
+    keypressEvents.onRightPressed.addListener(onRightPressed);
+}
+
+function onUpPressed() {
+    _player.moveUp();
+    render();
+}
+
+function onDownPressed() {
+    _player.moveDown();
+    render();
+}
+
+function onLeftPressed() {
+    _player.moveLeft();
+    render();
+}
+
+function onRightPressed() {
+    _player.moveRight();
+    render();
+}
+
+function render() {
+    _textCanvas.clear();
+
+    // TODO : position should be translated via Camera
+    _textCanvas.drawCharacter(_player.position.x, _player.position.y, "@");
+
+    console.clear();
+    console.log(_textCanvas.paint());
+}
+
+main();
