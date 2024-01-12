a36a8e22d18cf02d7ba4ee4679b8bc2594471035
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-12 09:43:49 +0800

    feat: ascii styling

--- packages/test-ascii-web/frontend/src/App.tsx
+++ packages/test-ascii-web/frontend/src/App.tsx
@@ -1,39 +1,44 @@
+import { useState } from "react";
+
 function App() {
   return (
-    <div className="h-full p-2 flex flex-col items-center bg-stone-800">
+    <div className="h-full p-2 flex flex-col items-center bg-stone-950">
 
-      <div className="pt-8">
-        <h1 className="text-stone-200 font-bold text-3xl">
-          <code>
-            ASCII RPG
-          </code>
+      <div className="pt-2">
+        <h1 className="hidden">
+          ASCII RPG
         </h1>
+        <code className="whitespace-pre-wrap text-stone-100 text-xs sm:text-base">
+          {
+            "____ ____ ____ _ _    ____ ___  ____ \n" +
+            "|__| [__  |    | |    |__/ |__] | __ \n" +
+            "|  | ___] |___ | |    |  \\ |    |__]"
+          }
+        </code>
       </div>
 
       <div className="flex-grow flex flex-col justify-center items-center">
         <code className="whitespace-pre-wrap text-stone-100" >
           {
-            "*****************************\n" +
-            "*****************************\n" +
-            "*****************************\n" +
-            "*****************************\n" +
-            "*****************************\n" +
-            "*****************************\n" +
-            "*****************************\n" +
-            "*****************************\n" +
-            "*****************************\n" +
-            "*****************************\n" +
-            "*****************************\n"
+            "******************************\n" +
+            "******************************\n" +
+            "******************************\n" +
+            "******************************\n" +
+            "******************************\n" +
+            "******************************\n" +
+            "******************************\n" +
+            "******************************\n" +
+            "******************************\n"
           }
         </code>
       </div>
 
-      <div className="pt-8 pb-4">
+      <div className="pb-2">
         <DPad />
       </div>
 
       <div className="w-full max-w-prose">
-        <p className="text-stone-500 text-end">
+        <p className="text-stone-500 text-end font-mono">
           GJTiquia
         </p>
       </div>
@@ -42,9 +47,9 @@ function App() {
 }
 
 function DPad() {
-  return <div className="flex flex-col items-center gap-2">
+  return <div className="flex flex-col items-center gap-3">
     <DButton />
-    <div className="flex gap-2">
+    <div className="flex gap-3">
       <DButton />
       <DButton />
       <DButton />
@@ -53,9 +58,36 @@ function DPad() {
 }
 
 function DButton() {
+  const [isPressed, setIsPressed] = useState(false);
+
+  const buttonAscii =
+    isPressed ?
+      "###########\n" +
+      "###########\n" +
+      "###########\n" +
+      "###########\n" +
+      "###########\n"
+      :
+      "###########\n" +
+      "#         #\n" +
+      "#         #\n" +
+      "#         #\n" +
+      "###########\n"
+    ;
+
+
   return <button
-    className="w-min p-12 border-2 border-stone-500 hover:bg-stone-500 hover:border-stone-500 active:bg-stone-400 active:border-stone-400"
-  />
+    className="text-stone-500"
+    onMouseDown={() => setIsPressed(true)}
+    onMouseUp={() => setIsPressed(false)}
+    onMouseLeave={() => setIsPressed(false)}
+    onTouchStart={() => setIsPressed(true)}
+    onTouchEnd={() => setIsPressed(false)}
+  >
+    <code className="whitespace-pre-wrap select-none">
+      {buttonAscii}
+    </code>
+  </button>
 }
 
 export default App
