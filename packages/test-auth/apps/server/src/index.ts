000bc8ac1de0ec31a31fdd74c522029664b10760
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-15 02:13:30 +0800

    chore: import jsonwebtoken to verify jwt

--- packages/test-auth/apps/server/src/index.ts
+++ packages/test-auth/apps/server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World")
+import dotenv from 'dotenv';
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+const jwtSecret = process.env.AUTH_JWT_SECRET as string;
+const token = "invalid token";
+
+try {
+    jwt.verify(token, jwtSecret);
+}
+catch (e) {
+    console.error("Error:", (e as Error).message)
+}
+
