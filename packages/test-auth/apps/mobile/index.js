dbe2775b7438e83b51b70cff663713bbcd66a87d
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-19 14:50:27 +0800

    WIP: attempt to fix by setting "true" monorepo root + custom entry script

---
+++ packages/test-auth/apps/mobile/index.js
@@ -0,0 +1,12 @@
+// https://docs.expo.dev/router/reference/troubleshooting/
+
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+    const ctx = require.context('./app');
+    return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
